c3488aba3ed1c908e8a2b7abc493193dccdc6546
Isaac-Vitale <85357842+Isaac-Vitale@users.noreply.github.com>
2022-06-02 09:56:52 -0300

    Add files via upload

---
+++ practica array js/main.js
@@ -0,0 +1,39 @@
+//1
+let estudiantes = ["Tom", "Ciro", "Sam", "Mily", "Lai"]
+document.write(estudiantes)
+
+//2
+console.log(estudiantes[4])
+
+//3
+estudiantes[2] = "Samuel";
+console.log(estudiantes)
+
+//4
+estudiantes.push("Albert");
+alert("Se agregó un alumno: "+ estudiantes[estudiantes.length -1])
+
+//5
+estudiantes.pop("Albert")
+console.log(estudiantes)
+
+//6
+estudiantes.unshift("Alberto")
+console.log(estudiantes)
+
+//7
+estudiantes.shift()
+console.log(estudiantes)   
+
+//8
+let ingresantes2023 = ["Yoel", "Samara", "Alexa"]
+nuevoAño = estudiantes.concat(ingresantes2023)
+console.log(nuevoAño)
+
+//9
+nuevoAño.sort()
+console.log(nuevoAño)
+
+//10
+let articulos = ["Gaseosa", "Gomitas", "Alfajor", "Jugo de naranja", "Queso"]
+
